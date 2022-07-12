18afa3311a978e7b13ff28d9166a4de3c39fa0ae
pavani <pavanibali@gmail.com>
2022-07-12 23:17:19 +0530

    added war changes

--- src/main/java/com/greatlearning/security/SpringBootSecurityApplication.java
+++ src/main/java/com/greatlearning/security/SpringBootSecurityApplication.java
@@ -2,12 +2,18 @@ package com.greatlearning.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
-public class SpringBootSecurityApplication {
+public class SpringBootSecurityApplication extends SpringBootServletInitializer {
+        @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SpringBootSecurityApplication.class);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSecurityApplication.class, args);
